4c58ae0974d1885ad80e4b575614f475ec0a7bb4
MedPass <medpass@MedPassServer.local>
2017-12-08 15:13:38 -0200

    Creating the that get users birthday, height and weight

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-        // $('.choose-ethnicity .female').fadeOut();
+        // CHOOSE ETHNICITY AND GENRE
 
         $('.choose-genre .female').click(function(event) {
                 $('.choose-genre .female img').toggleClass('active');
@@ -20,6 +20,44 @@ $(document).ready(function() {
                     $('.choose-ethnicity .male').fadeIn();
                     $('.choose-ethnicity .male').css('display','flex');
                 }, 500);
+        });
+
+        // SELECT INPUT
+
+        $('.md-select.day').on('click', function () {
+          $(this).toggleClass('active');
+        });
+
+        $('.md-select.day ul li').on('click', function () {
+          var v = $(this).text();
+          $('.md-select.day ul li').not($(this)).removeClass('active');
+          $(this).addClass('active');
+          $('.md-select.day label button').addClass('selected');
+          $('.md-select.day label button').text('Dia' + ' ' + v);
+        });
+
+        $('.md-select.month').on('click', function () {
+          $(this).toggleClass('active');
+        });
+
+        $('.md-select.month ul li').on('click', function () {
+          var v = $(this).text();
+          $('.md-select.month ul li').not($(this)).removeClass('active');
+          $(this).addClass('active');
+          $('.md-select.month label button').addClass('selected');
+          $('.md-select.month label button').text(v);
+        });
+
+        $('.md-select.year').on('click', function () {
+          $(this).toggleClass('active');
+        });
+
+        $('.md-select.year ul li').on('click', function () {
+          var v = $(this).text();
+          $('.md-select.year ul li').not($(this)).removeClass('active');
+          $(this).addClass('active');
+          $('.md-select.year label button').addClass('selected');
+          $('.md-select.year label button').text(v);
         });
 
 });
