ec4943d1faefefec54eb9614c643f578060901c9
MedPass <medpass@MedPassServer.local>
2017-12-21 17:15:08 -0200

    Adjustments on responsive at tablets devices and change alignments of containers

--- js/common-functions.js
+++ js/common-functions.js
@@ -1,3 +1,24 @@
+// REMOVE STYLES AT TAP EVENT ON MOBILE BROWSERS
+document.addEventListener("touchstart", function(){}, true);
+
+// DISABLE DOUBLE-TAP ZOOM AT MOBILE BROWSERS
+(function($) {
+  $.fn.nodoubletapzoom = function() {
+      $(this).bind('touchstart', function preventZoom(e) {
+        var t2 = e.timeStamp
+          , t1 = $(this).data('lastTouch') || t2
+          , dt = t2 - t1
+          , fingers = e.originalEvent.touches.length;
+        $(this).data('lastTouch', t2);
+        if (!dt || dt > 500 || fingers > 1) return; // not double-tap
+
+        e.preventDefault(); // double tap - prevent the zoom
+        // also synthesize click events we just swallowed up
+        $(this).trigger('click').trigger('click');
+      });
+  };
+})(jQuery);
+
 $(document).ready(function() {
 
 });
